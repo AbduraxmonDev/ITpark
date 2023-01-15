a54daf6d02e243cf2abc9eecf86033ecbc42eb6a
AbduraxmonDev <abduraxmonov000007@gmail.com>
2023-01-15 23:36:06 +0500

    second commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 let btn = document.querySelector('#burger');
+let link = document.querySelectorAll('#link');
+let links = document.querySelector('#header__navbar')
+console.log(link)
 btn.addEventListener('click', function() {
-    document.querySelector("header").classList.toggle("open")
+    document.querySelector('header').classList.toggle('open')
 });
 document.addEventListener('keydown', function(e){
     if (e.key == 'Escape') {
-        document.querySelector("header").classList.remove("open")
+        document.querySelector('header').classList.remove('open')
     }
-}) ;
-document.querySelectorAll('#header__link').addEventListener("click", function() {
-    document.querySelector("header").classList.remove("open")
-})
+});
+for (let links of link) {
+    links.addEventListener('click', function() {
+        document.querySelector('header').classList.remove('open')
+    });
+  }

72861b3dfc595a16f4ba719ffe4f26b2171607fa
AbduraxmonDev <abduraxmonov000007@gmail.com>
2023-01-17 11:27:33 +0500

    hi

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-let btn = document.querySelector('#burger');
-let link = document.querySelectorAll('#link');
-let links = document.querySelector('#header__navbar')
-
-btn.addEventListener('click', function() {
-    document.querySelector('header').classList.toggle('open')
+let btn = document.querySelector("#burger");
+let link = document.querySelectorAll("#link");
+btn.addEventListener("click", function () {
+  document.querySelector("header").classList.toggle("open");
 });
-document.addEventListener('keydown', function(e){
-    if (e.key == 'Escape') {
-        document.querySelector('header').classList.remove('open')
-    }
-});
-for (let links of link) {
-    links.addEventListener('click', function() {
-        document.querySelector('header').classList.remove('open')
-    });
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    document.querySelector("header").classList.remove("open");
   }
+});
+for (let item of link) {
+  item.addEventListener("click", function () {
+    document.querySelector("header").classList.remove("open");
+  });
+};
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});

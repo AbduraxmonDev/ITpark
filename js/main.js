df2521f76bc3ce67193edcb109b27ee32fa5a164
AbduraxmonDev <abduraxmonov000007@gmail.com>
2023-01-19 20:36:05 +0500

    hi

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@ for (let item of link) {
   item.addEventListener("click", function () {
     document.querySelector("header").classList.remove("open");
   });
-};
+}
 
-var swiper = new Swiper(".mySwiper", { 
+var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
@@ -24,10 +24,25 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  }
+  },
 });
 
-let moreBtn = document.querySelector('#moreBtn');
-moreBtn.addEventListener('click', function(e) {
+let moreBtn = document.querySelector("#moreBtn");
+moreBtn.addEventListener("click", function () {
   document.querySelector(".courses__types").classList.toggle("more");
-})
+});
+
+let input = document.querySelector("#input");
+let submitBtn = document.querySelector("#submitBtn");
+let error = document.querySelector("#error");
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (input.value === "") {
+    error.style.display = "block";
+    setTimeout(function () {
+      error.style.display = "none";
+    }, 3000);
+  } else {
+    console.log(typeof input.value)
+  }
+});
